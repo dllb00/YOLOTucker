5062d0ca1606a871400d480a541ef608ed1e0e0d
Dario Wirtz <dariowirtz@me.com>
2017-12-07 21:26:45 +0900

    1) lra implementation

--- lra.py
+++ lra.py
@@ -2,16 +2,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-proto_input = "/opt/caffe/examples/cifar10/cifar10_quick_train_test.prototxt" # Architecture of network (layer configuration) darknet: .cfg
-weight_input = "/opt/caffe/examples/cifar10/cifar10_quick_iter_5000.caffemodel.h5" # darknet: .weights
+proto_input = "/tiniy-yolo-caffe/tiny_yolo_voc.prototxt"
+weight_input = "/tiniy-yolo-caffe/tiny_yolo_voc.caffemodel"
 
-proto_output = "/opt/caffe/examples/cifar10/cifar10_quick_lra_train_text.prototxt"
-weight_output = "/opt/caffe/examples/cifar10/cifar10_quick_lra.caffemodel"
+proto_output = "/tiniy-yolo-caffe/tiny_yolo_voc_lra.prototxt"
+weight_output = "/tiniy-yolo-caffe/tiny_yolo_voc_lra.caffemodel"
 
 from proto_lra import proto_lra
 from weight_lra import weight_lra
 
-lra_map = {'conv1': (3,32), 'conv2': (16,32), 'conv3': (32, 64), 'ip1': 64, 'ip2': 10}
+lra_map = {'conv1': (3,32), 'conv2': (16,32), 'conv3': (32, 64)} #, 'ip1': 64, 'ip2': 10
 
 if __name__ == "__main__":
         proto_lra(proto_input, proto_output, lra_map)
