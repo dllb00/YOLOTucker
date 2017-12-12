27000a46a474a0fe3de36ea3b1704ba1acc44e2e
Dario Wirtz <dariowirtz@me.com>
2017-12-12 15:20:52 +0900

    missing files

--- lra.py
+++ lra.py
@@ -12,6 +12,8 @@ from proto_lra import proto_lra
 from weight_lra import weight_lra
 
 lra_map = {'conv1': (3,32), 'conv2': (16,32), 'conv3': (32, 64)} #, 'ip1': 64, 'ip2': 10
+#lra_map = {'conv1_scale': (3,16),'conv2_scale': (16,32),'conv3_scale': (32,64),'conv4_scale': (64,128),'conv5_scale': (128,256) ,'conv6_scale': (256,512),'conv7_scale': (512,1024),'conv8_scale': (1024,1024),'conv9': (1024,125)}
+#lra_map = {'conv1': (3,16),'conv2': (16,32),'conv3': (32,64),'conv4': (64,128),'conv5': (128,256) ,'conv6': (256,512),'conv7': (512,1024),'conv8': (1024,1024),'conv9': (1024,125)}
 
 if __name__ == "__main__":
         proto_lra(proto_input, proto_output, lra_map)
